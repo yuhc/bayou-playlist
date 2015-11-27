99de32716a2a1351946dbddbbdded25f23b9e987
Hangchen YU <yuhc123@gmail.com>
2015-11-27 16:46:23 -0600

    Lots of things needed reconsideration

--- src/api.py
+++ src/api.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 
 from threading import Thread, Lock
+from server    import Server
+from client    import Client
+
+servers = {} # list of servers
+clients = {} # list of clients
 
 def joinServer(server_id):
-    pass
+    # the first server is also the primary
+    if not servers:
+        servers[server_id] = Server(server_id)
+
+        s = servers[server_id]
+        s.unique_id  = (None, server_id)
+        s.is_primary = True
+        s.version_vector[s.unique_id] = 1
+    else:
+        servers[server_id] = Server(server_id)
+
+    # connect to all other servers in the system
+    for index in servers:
+        if index != server_id:
+            s.notify(index)
+
+
 
 
 def retireServer(server_id):
--- src/server.py
+++ src/server.py
@@ -5,7 +5,30 @@ import sys
 class Server:
 
     def __init__(self, node_id):
-        self.node_id = node_id
+        self.node_id    = node_id
+        self.unique_id  = None  # replica_id in lecture note
+
+        self.is_primary = False # first created server is the primary
+        self.is_retired = False
+
+        self.version_vector = {} # <server, clock>
+        self.CSN = 0            # commit sequence number
+        self.accept_time = 0
+
+        self.committed_log = []
+        self.tentative_log = []
+
+        self.nt = Network
+
+    '''
+    notify server @dest_id about its joining. '''
+    def notify(self, dest_id):
+        m_join_server = Message(server_id, None, "Creation", None)
+        self.nt.send_to_server(dest_id, m_join_server)
+        # TODO: while wait-for-ack
+
+    def __str__(self):
+        return "Server #" + self.node_id + "#" + self.unique_id
 
 
 if __name__ == "__main__":
