ef7b02ab06056587d86d6b46790973c78e9512ee
Hangchen YU <yuhc123@gmail.com>
2015-11-27 14:46:55 -0600

    Add the list of APIs

---
+++ src/api.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+from threading import Thread, Lock
+
+def joinServer(server_id):
+    pass
+
+
+def retireServer(server_id):
+    pass
+
+
+def joinClient(client_id, server_id):
+    pass
+
+
+def breakConnection(id1, id2):
+    pass
+
+
+def restoreConnection(id1, id2):
+    pass
+
+
+def pause():
+    pass
+
+
+def start():
+    pass
+
+
+def stabilize():
+    pass
+
+
+def printLog(server_id):
+    pass
+
+
+def put(client_id, song_name, url):
+    pass
+
+
+def get(client_id, song_name):
+    pass
+
+
+def delete(client_id, song_name):
+    pass
