c12b4e18b0eee52c336ebfc80fce039969fe56d5
Zhiting Zhu <zzt0215@uw.edu>
2015-11-29 17:35:05 -0600

    add GetAck documentation

--- src/message.py
+++ src/message.py
@@ -7,7 +7,7 @@ class Message:
     '''
     @mtype is the type of the message. It can be either
            TODO: rewrite this comment
-           from client: Get, Put, Delete,
+           from client: Get, Put, Delete, GetAck
            from server: Write, RequestAntiEn, AntiEntropy, AntiEn_Ack,
                         AntiEn_Finsh, Creation, Creation_Ack
            from master: Retire, Join, Break, Restore, Pause, Start, Print, Get,
