abb52c34cf851f1cbe89fa9acc335dc8d2d6788b
Hangchen YU <yuhc123@gmail.com>
2015-11-30 21:18:59 -0600

    Continue to move apis to a class

--- src/api.py
+++ src/api.py
@@ -27,46 +27,46 @@ class API:
         self.c_has_received_res = Condition()
 
         self.uid = "Master#0"
-        self.nt  = Network(uid)
+        self.nt  = Network(self.uid)
 
     def receive(self):
         while 1:
-            buf = nt.receive()
+            buf = self.nt.receive()
             if buf:
                 if TERM_LOG:
-                    print(uid, "handles:", str(buf))
+                    print(self.uid, "handles:", str(buf))
 
                 if buf.mtype == "Playlist":
-                    print(uid, buf.content)
-                    has_received_log = True
+                    print(self.uid, buf.content)
+                    self.has_received_log = True
 
                 elif buf.mtype == "MGetAck":
-                    print(uid, buf.content)
-                    c_has_received_res.acquire()
-                    has_received_res = True
-                    c_has_received_res.notify()
-                    c_has_received_res.release()
+                    print(self.uid, buf.content)
+                    self.c_has_received_res.acquire()
+                    self.has_received_res = True
+                    self.c_has_received_res.notify()
+                    self.c_has_received_res.release()
 
                 elif buf.mtype == "Done": # done processing put/delete
-                    c_has_received_res.acquire()
-                    has_retired_res = True
-                    print("in receive done", has_retired_res)
-                    c_has_received_res.notify()
-                    print("in receive done", has_retired_res)
-                    c_has_received_res.release()
+                    self.c_has_received_res.acquire()
+                    self.has_retired_res = True
+                    print("in receive done", self.has_retired_res)
+                    self.c_has_received_res.notify()
+                    print("in receive done", self.has_retired_res)
+                    self.c_has_received_res.release()
 
                 elif buf.mtype == "RetireAck":
-                    c_has_received_res.acquire()
-                    has_retired_res = True
-                    c_has_received_res.notify()
-                    c_has_received_res.release()
+                    self.c_has_received_res.acquire()
+                    self.has_retired_res = True
+                    self.c_has_received_res.notify()
+                    self.c_has_received_res.release()
 
     try:
         t_recv = Thread(target=receive)
         t_recv.daemon = True
         t_recv.start()
     except:
-        print(uid, "error: unable to start new thread")
+        print(self.uid, "error: unable to start new thread")
 
 
     def joinServer(server_id):
@@ -74,16 +74,16 @@ class API:
             # the first server is also the primary
             p = subprocess.Popen(["./src/server.py",
                                   str(server_id),
-                                  str(False) if servers else str(True)])
-            servers[server_id] = p.pid
-            nodes.append(server_id)
+                                  str(False) if self.servers else str(True)])
+            self.servers[server_id] = p.pid
+            self.nodes.append(server_id)
             #TODO: wait for ack
             time.sleep(1)
             if TERM_LOG:
                 print("Server#", server_id, " pid:", p.pid, sep="")
 
             # connect to a server in the system
-            for index in servers:
+            for index in self.servers:
                 if index != server_id:
                     m_create = Message(-1, None, "Creation", index)
                     nt.send_to_node(server_id, m_create)
@@ -91,29 +91,29 @@ class API:
 
 
     def retireServer(server_id):
-        if servers[server_id]:
+        if self.servers[server_id]:
             m_retire = Message(-1, None, "Retire", None)
-            nt.send_to_server(server_id, m_retire)
-            while not has_retired_res:
+            self.nt.send_to_server(server_id, m_retire)
+            while not self.has_retired_res:
                 pass
-            nodes.remove(server_id)
-            servers.pop(server_id)
+            self.nodes.remove(server_id)
+            self.servers.pop(server_id)
 
 
     def joinClient(client_id, server_id):
-        if not client_id in nodes:
-            if servers[server_id]:
+        if not client_id in self.nodes:
+            if self.servers[server_id]:
                 p = subprocess.Popen(["./src/client.py",
                                       str(client_id)])
-                clients[client_id] = p.pid
-                nodes.append(client_id)
+                self.clients[client_id] = p.pid
+                self.nodes.append(client_id)
                 #TODO: wait for ack
                 time.sleep(1)
                 if TERM_LOG:
                     print("Client#", client_id, " pid:", p.pid, sep="")
 
                 m_join = Message(-1, None, "Join", server_id)
-                nt.send_to_node(client_id, m_join)
+                self.nt.send_to_node(client_id, m_join)
             else:
                 if TERM_LOG:
                     print("Server#", server_id, " has not started", sep="")
@@ -123,37 +123,37 @@ class API:
 
 
     def breakConnection(id1, id2):
-        if id1 in nodes and id2 in nodes:
+        if id1 in self.nodes and id2 in self.nodes:
             m_break = Message(-1, None, "Break", id1)
-            nt.send_to_node(id2, m_break)
+            self.nt.send_to_node(id2, m_break)
             m_break = Message(-1, None, "Break", id2)
-            nt.send_to_node(id1, m_break)
+            self.nt.send_to_node(id1, m_break)
 
 
     def restoreConnection(id1, id2):
-        if id1 in nodes and id2 in nodes:
-            if id1 in server_list:
+        if id1 in self.nodes and id2 in self.nodes:
+            if id1 in self.server_list:
                 m_break = Message(-1, None, "Restore", ("Server", id1))
             else:
                 m_break = Message(-1, None, "Restore", ("Client", id1))
-            nt.send_to_node(id2, m_break)
-            if id2 in server_list:
+            self.nt.send_to_node(id2, m_break)
+            if id2 in self.server_list:
                 m_break = Message(-1, None, "Restore", ("Server", id2))
             else:
                 m_break = Message(-1, None, "Restore", ("Client", id2))
-            nt.send_to_node(id1, m_break)
+            self.nt.send_to_node(id1, m_break)
 
 
     def pause():
-        for index in servers:
+        for index in self.servers:
             m_pause = Message(-1, None, "Pause", None)
-            nt.send_to_node(index, m_pause)
+            self.nt.send_to_node(index, m_pause)
 
 
     def start():
-        for index in servers:
+        for index in self.servers:
             m_start = Message(-1, None, "Start", None)
-            nt.send_to_node(index, m_start)
+            self.nt.send_to_node(index, m_start)
 
 
     def stabilize():
@@ -161,10 +161,10 @@ class API:
 
 
     def printLog(server_id):
-        if server_id in servers:
+        if server_id in self.servers:
             m_print = Message(-1, None, "Print", None)
             self.has_received_log = False
-            nt.send_to_node(server_id, m_print)
+            self.nt.send_to_node(server_id, m_print)
             while not self.has_received_log:
                 pass
 
@@ -178,15 +178,15 @@ class API:
             print("after before", has_received_res)
             # while not has_received_res:
             #     pass
-            c_has_received_res.acquire()
+            self.c_has_received_res.acquire()
             print("acquires lock")
             while True:
                 if has_received_res:
                     break
-                c_has_received_res.wait()
+                self.c_has_received_res.wait()
                 print("in loop", has_received_res)
             print("XXXXXXXXXXXXXX Done")
-            c_has_received_res.release()
+            self.c_has_received_res.release()
 
 
     def get(client_id, song_name):
@@ -196,12 +196,12 @@ class API:
             nt.send_to_node(client_id, m_get)
             # while not has_received_res:
             #     pass
-            c_has_received_res.acquire()
+            self.c_has_received_res.acquire()
             while True:
                 if has_received_res:
                     break
-                c_has_received_res.wait()
-            c_has_received_res.release()
+                self.c_has_received_res.wait()
+            self.c_has_received_res.release()
 
     def delete(client_id, song_name):
         if client_id in clients:
@@ -210,12 +210,12 @@ class API:
             nt.send_to_node(client_id, m_delete)
             # while not has_received_res:
             #     pass
-            c_has_received_res.acquire()
+            self.c_has_received_res.acquire()
             while True:
                 if has_received_res:
                     break
-                c_has_received_res.wait()
-            c_has_received_res.release()
+                self.c_has_received_res.wait()
+            self.c_has_received_res.release()
 
 
     def exit():
