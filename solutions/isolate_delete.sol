0a3e816cab6fed203c7cdc1f85704ef9b036de05
Hangchen Yu <hyu@cs.utexas.edu>
2015-12-02 14:03:12 -0600

    Add isolate_delete.test and sol

---
+++ solutions/isolate_delete.sol
@@ -0,0 +1,12 @@
+eyesOfTexas:utexas.edu
+eyesOfTexas:cs.utexas.edu
+eyesOfTexas:ERR_KEY
+eyesOfTexas:cs.utexas.edu
+eyesOfTexas:cs.utexas.edu
+eyesOfTexas:cs.utexas.edu
+PUT:(eyesOfTexas utexas.edu):TRUE
+DELETE:(eyesOfTexas):TRUE
+PUT:(eyesOfTexas cs.utexas.edu):TRUE
+PUT:(eyesOfTexas utexas.edu):TRUE
+DELETE:(eyesOfTexas):TRUE
+PUT:(eyesOfTexas cs.utexas.edu):TRUE
